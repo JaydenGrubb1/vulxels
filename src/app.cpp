17faffa515402b98e5c4b49d55e024c00b07d71e
Jayden Grubb <jaydengrubb1@gmail.com>
2024-12-25 19:52:03 +1000

    Add sample rendering system (pipeline and render pass)

--- src/app.cpp
+++ src/app.cpp
@@ -9,13 +9,72 @@
 #include <vulxels/version.h>
 
 #include <cstdio>
+#include <glm/glm.hpp>
 #include <stdexcept>
 #include <thread>
 
 using namespace Vulxels;
 
 App::App() {
-	// TODO
+	auto vert = m_renderer.create_shader("simple.vert.spv");
+	auto frag = m_renderer.create_shader("simple.frag.spv");
+
+	vk::raii::RenderPass pass(
+		m_renderer.device().device(),
+		vk::RenderPassCreateInfo()
+			.setAttachments({vk::AttachmentDescription()
+								 .setFormat(m_renderer.swapchain().format().format)
+								 .setSamples(vk::SampleCountFlagBits::e1)
+								 .setLoadOp(vk::AttachmentLoadOp::eClear)
+								 .setStoreOp(vk::AttachmentStoreOp::eStore)
+								 .setStencilLoadOp(vk::AttachmentLoadOp::eDontCare)
+								 .setStencilStoreOp(vk::AttachmentStoreOp::eDontCare)
+								 .setInitialLayout(vk::ImageLayout::eUndefined)
+								 .setFinalLayout(vk::ImageLayout::ePresentSrcKHR)})
+			.setSubpasses({vk::SubpassDescription()
+							   .setPipelineBindPoint(vk::PipelineBindPoint::eGraphics)
+							   .setColorAttachments(
+								   {vk::AttachmentReference().setAttachment(0).setLayout(
+									   vk::ImageLayout::eColorAttachmentOptimal
+								   )}
+							   )})
+			.setDependencies(
+				{vk::SubpassDependency()
+					 .setSrcSubpass(VK_SUBPASS_EXTERNAL)
+					 .setDstSubpass(0)
+					 .setSrcStageMask(vk::PipelineStageFlagBits::eColorAttachmentOutput)
+					 .setDstStageMask(vk::PipelineStageFlagBits::eColorAttachmentOutput)
+					 .setSrcAccessMask(vk::AccessFlagBits::eNoneKHR)
+					 .setDstAccessMask(vk::AccessFlagBits::eColorAttachmentWrite)
+					 .setDependencyFlags(vk::DependencyFlagBits::eByRegion)}
+			)
+	);
+
+	struct Vert {
+		glm::vec2 pos;
+		glm::vec3 color;
+	};
+
+	auto pipeline =
+		m_renderer.create_pipeline()
+			.use_default()
+			.add_shader_stage(vert.module(), vk::ShaderStageFlagBits::eVertex)
+			.add_shader_stage(frag.module(), vk::ShaderStageFlagBits::eFragment)
+			.add_vertex_binding_description(0, sizeof(Vert), vk::VertexInputRate::eVertex)
+			.add_vertex_attribute_description(
+				0,
+				0,
+				vk::Format::eR32G32Sfloat,
+				offsetof(Vert, pos)
+			)
+			.add_vertex_attribute_description(
+				0,
+				1,
+				vk::Format::eR32G32B32Sfloat,
+				offsetof(Vert, color)
+			)
+			.set_render_pass(std::move(pass))
+			.build();
 }
 
 App::~App() {
