821be8d129652b8206449d8cee8a004efd0d8145
Jayden Grubb <jaydengrubb1@gmail.com>
2025-01-05 05:32:55 +1000

    Update example App to using vertex/index buffers

--- src/app.cpp
+++ src/app.cpp
@@ -5,6 +5,7 @@
  */
 
 #include <vulxels/app.h>
+#include <vulxels/gfx/buffer.h>
 #include <vulxels/types.h>
 #include <vulxels/version.h>
 
@@ -12,11 +13,27 @@
 #include <glm/glm.hpp>
 #include <stdexcept>
 #include <thread>
+#include <vector>
 
 using namespace Vulxels;
 
+struct Vertex {
+	glm::vec2 pos;
+	glm::vec3 color;
+};
+
 static std::shared_ptr<vk::raii::RenderPass> s_render_pass;
 static std::shared_ptr<GFX::Pipeline> s_pipeline;
+static std::shared_ptr<GFX::Buffer> s_vertex_buffer;
+static std::shared_ptr<GFX::Buffer> s_index_buffer;
+
+static std::vector<Vertex> s_vertices = {
+	{{-1.0f, -1.0f}, {1.0f, 0.0f, 0.0f}},
+	{{-1.0f, 1.0f}, {0.0f, 1.0f, 0.0f}},
+	{{1.0f, 1.0f}, {0.0f, 0.0f, 1.0f}},
+	{{1.0f, -1.0f}, {1.0f, 1.0f, 1.0f}}
+};
+static std::vector<u32> s_indices = {0, 1, 2, 2, 3, 0};
 
 App::App() {
 	auto vert = m_renderer.create_shader("simple.vert.spv");
@@ -61,8 +78,39 @@ App::App() {
 			.use_default()
 			.add_shader_stage(vert.module(), vk::ShaderStageFlagBits::eVertex)
 			.add_shader_stage(frag.module(), vk::ShaderStageFlagBits::eFragment)
+			.add_vertex_binding_description(
+				0,
+				sizeof(Vertex),
+				vk::VertexInputRate::eVertex
+			)
+			.add_vertex_attribute_description(
+				0,
+				0,
+				vk::Format::eR32G32Sfloat,
+				offsetof(Vertex, pos)
+			)
+			.add_vertex_attribute_description(
+				0,
+				1,
+				vk::Format::eR32G32B32Sfloat,
+				offsetof(Vertex, color)
+			)
 			.set_render_pass(s_render_pass)
 	);
+
+	s_vertex_buffer = std::make_shared<GFX::Buffer>(
+		m_renderer.device(),
+		sizeof(Vertex) * s_vertices.size(),
+		vk::BufferUsageFlagBits::eVertexBuffer
+	);
+	s_index_buffer = std::make_shared<GFX::Buffer>(
+		m_renderer.device(),
+		sizeof(u32) * s_indices.size(),
+		vk::BufferUsageFlagBits::eIndexBuffer
+	);
+
+	s_vertex_buffer->write(s_vertices);
+	s_index_buffer->write(s_indices);
 }
 
 static void draw(GFX::Renderer& renderer) {
@@ -95,8 +143,10 @@ static void draw(GFX::Renderer& renderer) {
 		{vk::Rect2D().setOffset({0, 0}).setExtent(renderer.swapchain().extent())}
 	);
 
+	cmd->bindVertexBuffers(0, {s_vertex_buffer->buffer()}, {0});
+	cmd->bindIndexBuffer(s_index_buffer->buffer(), 0, vk::IndexType::eUint32);
 	cmd->bindPipeline(vk::PipelineBindPoint::eGraphics, s_pipeline->pipeline());
-	cmd->draw(3, 1, 0, 0);
+	cmd->drawIndexed(s_indices.size(), 1, 0, 0, 0);
 
 	cmd->endRenderPass();
 
@@ -105,6 +155,8 @@ static void draw(GFX::Renderer& renderer) {
 
 App::~App() {
 	m_renderer.device().wait_idle();
+	s_index_buffer.reset();
+	s_vertex_buffer.reset();
 	s_pipeline.reset();
 	s_render_pass.reset();
 }
